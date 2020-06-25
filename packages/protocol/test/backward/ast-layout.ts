c2aac4f250cb6d56e442526e0949d12e14200638
Hernan Bandura <hbandura@gmail.com>
2020-06-25 16:13:03 -0300

    Add lib/**/* map and js files to gitignore Fixed ast layout backward compatibilities test

--- packages/protocol/test/backward/ast-layout.ts
+++ packages/protocol/test/backward/ast-layout.ts
@@ -1,14 +1,13 @@
+import { reportLayoutIncompatibilities } from '@celo/protocol/lib/backward/ast-layout'
 import { getBuildArtifacts } from '@openzeppelin/upgrades'
-
 /* HACK! truffle test was unable to compile this test (TypeScript would end up
  * claiming that it couldn't find the name `assert`) without the following 5
  * lines.
  */
 import { MigrationsContract } from 'types'
+
 // @ts-ignore
 const Migrations: MigrationsContract = artifacts.require('Migrations')
-
-import { reportLayoutIncompatibilities } from '@celo/protocol/lib/layout'
 
 /* We store artifacts for the various test cases in ./test/resources/layout
  * For each test case, there should be a build_<test case name> directory with
